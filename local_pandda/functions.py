8d3224d8593c0bfc4488e3251f09ba10652d586c
cwild <conor.wild@sky.com>
2021-03-12 23:29:16 +0000

    first

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -1115,9 +1115,9 @@ def get_affinity_background_corrected_density(
     dataset_shape = dataset_sample.shape
     fragment_shape = fragment_map.shape
 
-    dataset_min_x = max([0, index[0]-math.ceil(fragment_shape[0]/2)])
-    dataset_min_y = max([0, index[1]-math.ceil(fragment_shape[1]/2)])
-    dataset_min_z = max([0, index[2]-math.ceil(fragment_shape[2]/2)])
+    dataset_min_x = max([0, index[0]-math.floor(fragment_shape[0]/2)])
+    dataset_min_y = max([0, index[1]-math.floor(fragment_shape[1]/2)])
+    dataset_min_z = max([0, index[2]-math.floor(fragment_shape[2]/2)])
     dataset_max_x = min([dataset_shape[0], index[0]+math.ceil(fragment_shape[0]/2)])
     dataset_max_y = min([dataset_shape[1], index[1]+math.ceil(fragment_shape[1]/2)])
     dataset_max_z = min([dataset_shape[2], index[2]+math.ceil(fragment_shape[2]/2)])
@@ -1125,9 +1125,9 @@ def get_affinity_background_corrected_density(
     sample_overlap = dataset_sample[dataset_min_x:dataset_max_x, dataset_min_y:dataset_max_y, dataset_min_z:dataset_max_z, ]
     mean_overlap = mean[dataset_min_x:dataset_max_x, dataset_min_y:dataset_max_y, dataset_min_z:dataset_max_z, ]
 
-    fragment_min_x = max([0,  math.ceil(fragment_map.shape[0]/2)-index[0]])
-    fragment_min_y = max([0,  math.ceil(fragment_map.shape[1]/2)-index[1]])
-    fragment_min_z = max([0,  math.ceil(fragment_map.shape[2]/2)-index[2]])
+    fragment_min_x = max([0,  index[0]-math.floor(fragment_map.shape[0]/2)])
+    fragment_min_y = max([0,  index[0]-math.floor(fragment_map.shape[1]/2)])
+    fragment_min_z = max([0,  index[0]-math.floor(fragment_map.shape[2]/2)])
     fragment_max_x = min([fragment_shape[0], math.ceil(fragment_shape[0]/2)+(dataset_shape[0]-index[0])])
     fragment_max_y = min([fragment_shape[1], math.ceil(fragment_shape[1]/2)+(dataset_shape[1]-index[1])])
     fragment_max_z = min([fragment_shape[2], math.ceil(fragment_shape[2]/2)+(dataset_shape[2]-index[2])])
