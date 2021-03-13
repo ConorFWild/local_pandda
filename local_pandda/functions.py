dbcf669f281ee17cb143e3928b779c0fca3e75a4
cwild <conor.wild@sky.com>
2021-03-13 11:27:35 +0000

    first

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -1127,11 +1127,11 @@ def get_affinity_background_corrected_density(
     sample_overlap = dataset_sample[dataset_min_x:dataset_max_x, dataset_min_y:dataset_max_y, dataset_min_z:dataset_max_z, ]
     mean_overlap = mean[dataset_min_x:dataset_max_x, dataset_min_y:dataset_max_y, dataset_min_z:dataset_max_z, ]
 
-    fragment_min_x = max([0,  math.floor((fragment_map.shape[0]-2)/2)-index[0]])
+    fragment_min_x = max([0,  math.floor((fragment_map.shape[0]-1)/2)-index[0]])
     print([0,  math.floor(fragment_map.shape[0]/2)-index[0]])
     fragment_min_y = max([0,  math.floor((fragment_map.shape[1]-2)/2)-index[1]])
     fragment_min_z = max([0,  math.floor((fragment_map.shape[2]-2)/2)-index[2]])
-    fragment_max_x = min([fragment_shape[0], math.ceil(fragment_shape[0]/2)+(dataset_shape[0]-index[0])])
+    fragment_max_x = min([fragment_shape[0], math.ceil((fragment_shape[0]-1)/2)+(dataset_shape[0]-index[0])])
     print([fragment_shape[0], math.ceil(fragment_shape[0]/2)+(dataset_shape[0]-index[0])])
     fragment_max_y = min([fragment_shape[1], math.ceil(fragment_shape[1]/2)+(dataset_shape[1]-index[1])])
     fragment_max_z = min([fragment_shape[2], math.ceil(fragment_shape[2]/2)+(dataset_shape[2]-index[2])])
