05f7cb3574dfe5f5bec1879fd17d398ca0ee5d96
cwild <conor.wild@sky.com>
2021-03-23 13:32:11 +0000

    p

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -2909,7 +2909,7 @@ def analyse_dataset_gpu(
 
                 write_event_map(
                     event_map,
-                    out_dir / f"{dataset.dtag}_{marker.x}_{marker.y}_{marker.z}_{fragment_id}.ccp4",
+                    out_dir / f"{dataset.dtag}_{marker.x}_{marker.y}_{marker.z}_{fragment_id}.mtz",
                     dataset_event_marker,
                     dataset,
                     resolution,
