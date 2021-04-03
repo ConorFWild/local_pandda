926c789890d26830c4da2b5a09737e8e47f252d0
cwild <conor.wild@sky.com>
2021-04-03 23:22:12 +0100

    y

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -4129,8 +4129,8 @@ def analyse_residue_gpu(
         # if dtag != "HAO1A-x0964":
         #     continue
 
-        if dtag != "HAO1A-x0132":
-            continue
+        # if dtag != "HAO1A-x0132":
+        #     continue
 
         # if dtag != "HAO1A-x0808":
         #     continue
