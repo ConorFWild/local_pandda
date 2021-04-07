b43944d401a235ab174d98e213b8a2b8e5da45c9
cwild <conor.wild@sky.com>
2021-04-07 13:57:31 +0100

    y

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -2993,9 +2993,7 @@ def fragment_search_rmsd_gpu(xmap_np, fragment_maps_np, fragment_masks_np,
     print(f"rmsd: {rmsd.shape} {rmsd[0, 0, 24, 24, 24]}")
 
     min_index = np.unravel_index(torch.argmin(rmsd).cpu(), rmsd.shape)
-    print(f"\t{rho_o_squared[min_index[0], min_index[1], min_index[2], min_index[3], min_index[4]]} {rho_c_rho_c[min_index[0], min_index[1], min_index[2], min_index[3], min_index[4]]} {conv_rho_o_rho_c[min_index[0], min_index[1], min_index[2], min_index[3], min_index[4]]}")
-
-
+    print(f"\t{rho_o_squared[min_index[0], min_index[1], min_index[2], min_index[3], min_index[4]]} {rho_c_rho_c[0, min_index[1], 0, 0, 0]} {conv_rho_o_rho_c[min_index[0], min_index[1], min_index[2], min_index[3], min_index[4]]}")
 
     rmsd = torch.nan_to_num(rmsd, nan=0.0, posinf=0.0, neginf=0.0, )
 
