0c5055c68e71f718a001941640a8e66c391e8cc8
cwild <conor.wild@sky.com>
2021-04-08 16:13:21 +0100

    y

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -3083,10 +3083,11 @@ def fragment_search_z_gpu(xmap_np, fragment_masks_np,
     z_signal_unscaled = torch.nn.functional.conv3d(z, masks, padding=padding)
     print(f"z_signal_unscaled: {z_signal_unscaled.shape} {z_signal_unscaled[0, 0, 24, 24, 24]}")
 
-    z_signal_unscaled = z_signal_unscaled / torch.mean(z_signal_unscaled, 1)
-
     z_signal = z_signal_unscaled / size
     print(f"signal: {z_signal.shape} {z_signal[0, 0, 24, 24, 24]}")
+
+    z_signal = z_signal / torch.mean(z_signal, 1)
+
 
     z_signal = torch.nan_to_num(z_signal, nan=0.0, posinf=0.0, neginf=0.0, )
 
