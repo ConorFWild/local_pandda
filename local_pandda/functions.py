42ee2c14cbfb2695fc94edeb024c4f047652b577
cwild <conor.wild@sky.com>
2021-04-08 17:01:13 +0100

    y

--- local_pandda/functions.py
+++ local_pandda/functions.py
@@ -3087,6 +3087,7 @@ def fragment_search_z_gpu(xmap_np, fragment_masks_np,
     z_signal = z_signal_unscaled / size
     print(f"signal: {z_signal.shape} {z_signal[0, 0, 24, 24, 24]}")
 
+    z_signal = z_signal*(z_signal / torch.mean(z_signal, 1))
 
     z_signal = torch.nan_to_num(z_signal, nan=0.0, posinf=0.0, neginf=0.0, )
 
